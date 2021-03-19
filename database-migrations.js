aa89be9d52a11bdeaed46e3a176460d09a98165a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-03-19 11:08:37 +0100

    better file name

---
+++ database-migrations.js
@@ -0,0 +1,16 @@
+const sqlite = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+async function setup() {
+  try {
+    const db = await sqlite.open({
+      filename: "./games.sqlite",
+      driver: sqlite3.Database,
+    });
+    await db.migrate();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+setup();
