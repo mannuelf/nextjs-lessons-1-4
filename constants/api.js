f2af5ccf00e84a32e10db1d06c2c7324597c8b4a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-03-25 12:07:48 +0100

    demo

--- constants/api.js
+++ constants/api.js
@@ -1,2 +1,2 @@
 export const REACT_APP_BASE_URL = `${process.env.REACT_APP_RAWG_API}`;
-export const API_KEY = `${process.env.REACT_APP_RAWG_API_KEY}`;
+export const REACT_APP_API_KEY = `${process.env.REACT_APP_RAWG_API_KEY}`;
